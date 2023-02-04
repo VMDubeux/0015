38ed853c7eca2226e2dd8cd95a84b66028f4c66b
Victor Dubeux <adv.victordubeux@gmail.com>
2023-02-04 14:45:24 -0300

    P15 C2 - Envio definitivo de projeto

---
+++ ProjetoQuinze/ProjetoQuinze/Funcionario.cs
@@ -0,0 +1,19 @@
+﻿namespace ProjetoQuinze
+{
+    internal class Funcionario
+    {
+        public string Name;
+        public double AllSalary;
+        public double Tax;
+        public double IncreasePerc;
+
+        public double LiqSalary()
+        {
+            return AllSalary - Tax;
+        }
+        public double IncreasedSal()
+        {
+            return LiqSalary() + (AllSalary * (IncreasePerc / 100.0));
+        }
+    }
+}
---
+++ ProjetoQuinze/ProjetoQuinze/Program.cs
@@ -0,0 +1,30 @@
+﻿using ProjetoQuinze;
+using System;
+using System.Globalization;
+namespace Exercicio
+{
+    class ProjetoQuinze
+    {
+        static void Main(string[] args)
+        {
+            Funcionario f = new Funcionario();
+
+            Console.Write("Nome: ");
+            f.Name = Console.ReadLine();
+            Console.Write("Salário Bruto: ");
+            f.AllSalary = double.Parse(Console.ReadLine());
+            Console.Write("Imposto: ");
+            f.Tax = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.Write($"Funcionário: {f.Name}, salário líquido: R${f.LiqSalary()}.");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.Write("Aumento salarial proposto: ");
+            f.IncreasePerc = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.Write($"Dados atualizados: {f.Name}, R${f.IncreasedSal().ToString("F2", CultureInfo.InvariantCulture)}");
+            
+            Console.ReadKey();
+        }
+    }
+}
